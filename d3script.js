35b6ff5c6f8db442c0c2372f29a18f525d6137fe
Philip Hradecsni <baumzeit@posteo.me>
2019-03-07 15:43:27 +0100

    add message box, tooltip and improve display data actualization

--- d3script.js
+++ d3script.js
@@ -8,30 +8,49 @@ window.onload = () => {
             data.forEach(entry => {
                 const [ m, d, y ] = entry.date.split('/');
                 const dateString = [ y, m, d ].join('-');
+                entry.date = moment(dateString);
                 entry.weekday = moment(dateString).isoWeekday();
-                delete entry.date;
-                delete entry.time;
             });
-            visualizeData(data);
+
+            // data.filter(entry => entry.date.year(2017))
+            visualizeData(data.reverse());
         });
 
     function visualizeData(data) {
         console.log(data);
 
         xScale = d3.scaleOrdinal().domain()
+
+
         
         const w = 840;
         const h = 240;
 
         const svg = d3.select('#dataviz')
             .append('svg')
+            .attr('id', '#viz-svg')
             .attr('width', w)
             .attr('height', h)
-            .attr('id', '#viz-svg');
+
+        const statBar = d3.select('#dataviz')
+            .append('svg')
+            .attr('id', 'statbar')
+            .attr('width', w)
+            .attr('height', 20)
+
+        
+            
+        const tooltip = statBar
+            .append('text')
+            .attr('id', 'tooltip')
+            .attr('x', 0)
+            .attr('y', 15)
+            .attr('text-anchor', 'middle')
+            .style('opacity', 0);
 
         const barEnter = svg.selectAll('rect')
             .data(data)
-            .enter()
+            .enter();
 
         barEnter
             .append('rect')
@@ -40,17 +59,8 @@ window.onload = () => {
             .attr('x', (d, i) => i * 2)
             .attr('y', d => h - 100)
             .attr('width', 1)
-            .attr('height', 100)
+            .attr('height', h/2)
             .attr('data-title', (d, i) => d.title)
-
-        function selector(bool) {
-            return function(d, i) {
-                const id = '#bar-' + i;
-                d3.select(id).classed('selected', bool);
-            }
-        }
-        const selectBar = selector(true);
-        const deselectBar = selector(false);
 
         barEnter
             .append('rect')
@@ -59,31 +69,63 @@ window.onload = () => {
             .attr('y', 0)
             .attr('width', 1)
             .attr('height', h)
-            .on('mouseenter', selectBar)
-            .on('mouseout', deselectBar)
+            .on('mouseenter', updateSelect)
+            .on('mouseout', updateSelect)
+
+        function updateSelect(d, i) {
+            const id = '#bar-' + i;
+            d3.selectAll('.bar.selected').classed('selected', false)
+            d3.select(id).classed('selected', true);
+            d3.select('#message-date').html(`${d.date.format('MM/DD/YYYY, ddd')} ${d.time} (US-PDT)`);
+            d3.select('#message').html(`"${d.title}"`);
+
+            const dataBar = d3.select(id);
+            d3.select('#tooltip')
+                .html(dataBar.attr('data-stat'))
+                .attr('x', +dataBar.attr('x') + 6)
+                .style('opacity', 1)
+
+        }
 
 
         const filterKeys = Object.keys(data[0])
             .filter(d => d === 'clicks' || d === 'reach' || d === 'reactions');
 
-        d3.select('#controls').selectAll('button.stats')
+        d3.select('#controls').selectAll('button.filter-stats')
             .data(filterKeys)
             .enter()
             .append('button')
+            .attr('class', 'filter-stats')
+            .attr('id', d => d)
             .on('click', buttonClick)
             .html(d => d);
 
         function buttonClick(datapoint) {
-            const maxValue = d3.max(data, d => parseInt(d[datapoint]));
+            const maxValue = d3.max(data, d => +d[datapoint]);
             const heightScale = d3.scaleLinear()
                 .domain([ 0, maxValue ])
                 .range([ 0, h ]);
-            svg.selectAll('.bar').transition().delay(100).duration(1000)
-                .attr('y', d => h - heightScale(d[datapoint]))
-                .attr('height', d => heightScale(d[datapoint]))
+            svg.selectAll('.bar')
+                .attr('data-stat', d => +d[datapoint] + ' ' + datapoint)
+                .transition().delay(100).duration(1000)
+                .attr('y', d => h - heightScale(+d[datapoint]))
+                .attr('height', d => heightScale(+d[datapoint]))
+            const newStat = d3.select('.bar.selected').empty() 
+                ? '' 
+                : d3.select('.bar.selected').attr('data-stat');
+            d3.select('#tooltip').html(newStat);
+            d3.selectAll('.filter-stats').classed('active', false);
+            d3.select(`.filter-stats#${datapoint}`).classed('active', true);
         }
 
+        d3.select('.filter-stats')
+            .dispatch('click')
+            .classed('active', true)
 
+        const barBoxes = document.getElementsByClassName('bar-box');
+        const randomBox = barBoxes[Math.floor(Math.random() * barBoxes.length)];
+        const mouseenterEvent = new Event('mouseenter');
+        randomBox.dispatchEvent(mouseenterEvent);
     }
 
 }
